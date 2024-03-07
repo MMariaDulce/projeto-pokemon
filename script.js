f3cc9acbecb88b259382bbb4d36e1b31ff2de365
MMariaDulce <138811905+MMariaDulce@users.noreply.github.com>
2024-03-07 10:33:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-document.getElementById("Login").addEventListener("submit", function(event) {
-    event.preventDefault(); // Previne o envio do formulário
-  
-    // Obtenha os valores dos campos de entrada
-    const Nome = document.getElementById("login").value;
-    const Senha = document.getElementById("senha").value;
-  
-    // Aqui você faria a validação do login e senha
-    // Por exemplo, você pode compará-los com valores armazenados em algum lugar ou realizar uma solicitação de verificação para um servidor
-    if (Nome.trim() !== "" && Senha.trim() !== "") {
-    // Exemplo de validação simples
-    if (Nome !== Senha) 
-      alert("Login Aprovado!");
-      // Redirecionar para outra página ou executar outras ações após o login bem-sucedido
-    }
-  });
-  
+const menu = document.querySelector('.mobile-btn');
+const NavMenu = document.querySelector('.mobile-menu');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+})
